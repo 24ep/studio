2e925ceeed973158e49339e2180032958f7d4626
Jaroonwit Poolnai <jaroonwit.poo@qsncc.com>
2025-06-27 16:48:35 +0700

    debug

--- prisma/migrations/20250101000000_add_user_authentication_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "authentication_method" TEXT DEFAULT 'basic',
-ADD COLUMN "force_password_change" BOOLEAN DEFAULT false;
-
--- CreateIndex
-CREATE INDEX "User_authentication_method_idx" ON "User"("authentication_method"); 
