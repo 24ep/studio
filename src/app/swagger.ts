2a84c9f7f53b6336c6ec44faeabbf6fb4a643dd7
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2025-06-29 20:58:53 +0700

    debug

---
+++ src/app/swagger.ts
@@ -0,0 +1,16 @@
+const swaggerSpec = {
+  openapi: "3.0.0",
+  info: {
+    title: "CandiTrack API",
+    version: "1.0.0",
+    description: "API documentation for CandiTrack"
+  },
+  servers: [
+    { url: "http://localhost:3000", description: "Local server" }
+  ],
+  paths: {
+    // Add your endpoint definitions here
+  }
+};
+
+export default swaggerSpec; 
