4752b2481d514a6d0205cbccf03fb00ed552919f
jaroonwit poolnai <jaroonwit.pool@gmail.com>
2025-07-03 23:39:35 +0700

    debug

--- src/app/api/auth/validate-session/route.ts
+++ src/app/api/auth/validate-session/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth/next';
 import { authOptions, validateUserSession } from '@/lib/auth';
 
+export const dynamic = "force-dynamic";
+
 /**
  * @openapi
  * /api/auth/validate-session:
