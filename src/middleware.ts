0bdbc045b572eea42f9c5c9b7becfd89f7402af5
Jaroonwit Poolnai <jaroonwit.poo@qsncc.com>
2025-07-03 13:55:58 +0700

    debug

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from 'next-auth/jwt';
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 
 const protectedRoutes = [
   "/api/protected", // Add your protected endpoints here
